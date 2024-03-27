2f922ade0771f203954bec5024263692dcc0bcb6
Geunbeom <160552393+Geunbeom@users.noreply.github.com>
2024-03-27 22:53:11 +0900

    [Gold III] Title: 파티, Time: 712 ms, Memory: 83336 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,25 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Node implements Comparable<Node>{
-	int idx,w;
-	
-	Node() {}
-	
-	Node (int idx, int w) {
+class Node implements Comparable<Node> {
+	int idx, w;
+
+	Node() {
+	}
+
+	Node(int idx, int w) {
 		this.idx = idx;
 		this.w = w;
 	}
-	
+
 	@Override
 	public int compareTo(Node o) {
-		return Integer.compare(this.w, o.w);
+		return this.w - o.w;
 	}
 }
-
 
 public class Main {
 
@@ -29,86 +30,115 @@ public class Main {
 	static ArrayList<Node>[] graph, reverseGraph;
 	static int[] dist, reverseDist;
 	static boolean[] visited;
-	
+
 	public static void main(String[] args) throws Exception {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		st = new StringTokenizer(br.readLine());
-		
+
+		// N : 노드의 갯수, M : 간선의 갯수, X : 목표노드
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
 		int X = Integer.parseInt(st.nextToken());
-		
+
+		int INF = Integer.MAX_VALUE;
 		int max = 0;
-		
-		graph = new ArrayList[N+1];
-		reverseGraph = new ArrayList[N+1];
-		dist = new int[N+1];
-		reverseDist = new int[N+1];
-		visited = new boolean[N+1];
-		
-		for (int i=0; i<=N; i++) {
+
+		// 일반 그래프와 역방향 그래프
+		graph = new ArrayList[N + 1];
+		reverseGraph = new ArrayList[N + 1];
+
+		// 일반 dp 와 역방향 dp
+		dist = new int[N + 1];
+		reverseDist = new int[N + 1];
+
+		// 방문배열
+		visited = new boolean[N + 1];
+
+		// 최댓값으로 채운다
+		Arrays.fill(dist, INF);
+		Arrays.fill(reverseDist, INF);
+
+		dist[X] = 0;
+		reverseDist[X] = 0;
+
+		for (int i = 0; i <= N; i++) {
 			graph[i] = new ArrayList<Node>();
 			reverseGraph[i] = new ArrayList<Node>();
 		}
-		
-		for (int i=0; i<M; i++) {
+
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
+
+			// a 는 startNode, b 는 EndNode, w 는 가중치
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			
+
+			// 일반에는 입력대로 넣고, 역방향에는 반대로 넣는다.
 			graph[a].add(new Node(b, w));
 			reverseGraph[b].add(new Node(a, w));
-			
-			// 각각의 노드에서 X 까지 최단경로
-			// X 에서 각각 노드 까지의 최단경로
-			// 역방향그래프 : X 에서 각각의 최단경로 = 각각에서 X 의 최단경로
 		}
-		
-		dijkstra(X, dist, graph);
-		visited = new boolean[N+1];
-		dijkstra(X, reverseDist, reverseGraph);
 
-		for (int i=1; i<=N; i++) {
+		Node startNode = new Node(X, 0);
+
+		// 일반 노드를 이용한 다익스트라.
+		pq.offer(startNode);
+		dijkstra(dist, graph);
+
+		// 방문배열을 false 로 초기화
+		Arrays.fill(visited, false);
+
+		// 역방향 다익스트라.
+		pq.offer(startNode);
+		dijkstra(reverseDist, reverseGraph);
+
+		// 최단 경로의 합 중 최대치를 max 에 담는다.
+		for (int i = 1; i <= N; i++) {
+			if (dist[i] == INF)
+				dist[i] = 0;
+			if (reverseDist[i] == INF)
+				reverseDist[i] = 0;
+
 			dist[i] += reverseDist[i];
-			
+
 			if (dist[i] > max) {
 				max = dist[i];
 			}
-			
 		}
+
 		System.out.println(max);
-		
+
 	}
-	
-	static void dijkstra(int start, int[] dp, ArrayList<Node>[] graph1) {
-		Node startNode = new Node(start, 0);
-		pq.offer(startNode);
-		
+
+	// 다익스트라. dp, graph 를 parameter로 담는다.
+	static void dijkstra(int[] dp, ArrayList<Node>[] graph1) {
+
+		int idx = 0;
+		int next = 0;
+		int w = 0;
+
 		while (!pq.isEmpty()) {
-			
+
 			Node now = pq.poll();
-			int idx = now.idx;
+			idx = now.idx;
 			visited[idx] = true;
-			
+
 			for (Node nextNode : graph1[idx]) {
-				
-				if (visited[nextNode.idx]) continue;
-				
-				int next = nextNode.idx;
-				int w = nextNode.w;
-				
-				if (dp[next] == 0 || dp[next] > dp[idx] + w) {
+
+				if (visited[nextNode.idx])
+					continue;
+
+				next = nextNode.idx;
+				w = nextNode.w;
+
+				if (dp[next] > dp[idx] + w) {
 					dp[next] = dp[idx] + w;
 				}
 				pq.offer(new Node(next, dp[next]));
 			}
-	
+
 		}
-		
-		
+
 	}
-	
 
 }
