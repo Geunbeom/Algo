9ed5792e5abfb5b3ca108504ae26c21fc8333537
Geunbeom <160552393+Geunbeom@users.noreply.github.com>
2024-07-17 12:26:24 +0900

    [Gold II] Title: 보석 도둑, Time: 1160 ms, Memory: 135756 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,12 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Product implements Comparable<Product>{
+class Product {
     int w;
     int v;
 
@@ -15,10 +11,6 @@ class Product implements Comparable<Product>{
         this.v = v;
     }
 
-    @Override
-    public int compareTo(Product p) {
-        return Integer.compare(this.w, p.w);
-    }
 
 }
 
@@ -31,57 +23,51 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         long sum = 0;
+
+        // 낮은 무게 순 pq
         PriorityQueue<Product> pq = new PriorityQueue<>((p1, p2) -> {
-        	return Integer.compare(p1.w, p2.w);
+            return p1.w-p2.w;
         });
-        PriorityQueue<Product> pq2 = new PriorityQueue<>((p1, p2) -> {
-        	return Integer.compare(p2.v, p1.v);
+
+        // 가치가 높은 순의 pq
+        PriorityQueue<Integer> pq2 = new PriorityQueue<>((v1, v2) -> {
+            return v2-v1;
         });
-        int[] bags = new int[K];
-        
+
+        // 가방의 pq
+        PriorityQueue<Integer> pq3 = new PriorityQueue<>((b1, b2) -> {
+            return b1-b2;
+        });
+
         // 입력
         for (int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            
-            // pq 에 무게 순으로 저장
             pq.offer(new Product(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
         }
-        
+
         // 가방 값 입력
         for (int i=0; i<K; i++) {
             st = new StringTokenizer(br.readLine());
-            bags[i] = Integer.parseInt(st.nextToken());
+            pq3.offer(Integer.parseInt(st.nextToken()));
         }
-        
-        // 정렬
-        Arrays.sort(bags);
+
         // pq 에서 무게가 적은 것부터 빼고 높은 가치 순으로 정렬된 pq2 에 넣는다.
         int idx = 0;
-        while(idx < K) {
-        	
-        	if (!pq.isEmpty()) {
-        		if (bags[idx] >= pq.peek().w) {
-                    pq2.offer(pq.poll());
-        			
-        		}else if(pq2.isEmpty()){
-        			idx++;
-        			
-        		}else {
-        			
-        			// 만일 무게가 넘어가면 idx 를 증가.
-        			sum += pq2.poll().v;
-        			idx++;
-        		}
-        		
-        	}else if (!pq2.isEmpty()){
-        		sum += pq2.poll().v;
-    			idx++;
-    			
-        	}else break;
-        	
+        for (int i=0; i<K; i++) {
+            int nowB = pq3.poll();
+            while (!pq.isEmpty() && nowB >= pq.peek().w) {
+                pq2.offer(pq.poll().v);
+            }
+//  근범오빠 정처기 화이팅
+//  근범오빠 취업 화이팅
+//  좋겠다 알고리즘 잘해서
+//  제 코데 대신 봐주세요...Please
+            if (!pq2.isEmpty()) {
+                sum += pq2.poll();
+            }
 
         }
-        
+
         System.out.println(sum);
 
     }
