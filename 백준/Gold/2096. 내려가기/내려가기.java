97360f2f6f4b8cfcb93b0ef534a1e709e5cf27bc
Seo Geun-Beom <160552393+Geunbeom@users.noreply.github.com>
2025-01-11 03:27:28 +0900

    [Gold V] Title: 내려가기, Time: 332 ms, Memory: 44340 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -4,41 +4,42 @@ import java.io.*;
 public class Main {
 
 	static int[] d = {-1, 0, 1};
+	static StringTokenizer st;
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
-		int[][][] arr = new int[N][3][2];
-		int[] answer = new int[2];
-		answer[1] = Integer.MAX_VALUE;
+		int[][] curr = new int[3][2];
+		int[][] next = new int[3][2];
+		boolean change = false;
 		
 		for (int r=0; r<N; r++) {
 			st = new StringTokenizer(br.readLine());
-			for (int c=0; c<3; c++) {
-				int input = Integer.parseInt(st.nextToken());
-				arr[r][c][0] += input;
-				arr[r][c][1] += input;
-				if(r == N-1) continue;
-				
-				int C = 0;
-				for (int i=0; i<3; i++) {
-					C = c + d[i]; 
-					if (C < 0 || C > 2) continue;
-					arr[r+1][C][0] = Math.max(arr[r+1][C][0], arr[r][c][0]);
-					arr[r+1][C][1] = arr[r+1][C][1] == 0 ? arr[r][c][1] :
-									Math.min(arr[r+1][C][1], arr[r][c][1]);
-					
-				}
-				
-			}
-		}
-		for (int i=0; i<3; i++) {
-			answer[0] = Math.max(arr[N-1][i][0], answer[0]);
-			answer[1] = Math.min(arr[N-1][i][1], answer[1]);
+			change = change ? false : true;
+			if (change) todo(curr, next);
+			else todo(next, curr);
 		}
 		
-		System.out.println(answer[0] + " " + answer[1]);
+		System.out.println(
+			change ? Math.max(Math.max(next[0][0], next[1][0]), next[2][0]) 
+					+ " " + Math.min(Math.min(next[0][1], next[1][1]), next[2][1]) :
+					Math.max(Math.max(curr[0][0], curr[1][0]), curr[2][0]) 
+					+ " " + Math.min(Math.min(curr[0][1], curr[1][1]), curr[2][1])
+		);
+	}
+	
+	static public void todo(int[][] curr, int[][] next) {
+		for (int c=0; c<3; c++) {
+			int input = Integer.parseInt(st.nextToken());
+			next[c][1] = Integer.MAX_VALUE;
+			for (int i=0; i<3; i++) {
+				int C = c + d[i]; 
+				if (C < 0 || C > 2) continue;
+				next[c][0] = Math.max(curr[C][0]+input, next[c][0]);
+				next[c][1] = Math.min(curr[C][1]+input, next[c][1]);
+			}
+		}
 	}
 }
