4df074b0bfc9b794b009733abf33a18de80f23e8
Seo Geun-Beom <160552393+Geunbeom@users.noreply.github.com>
2025-02-06 22:57:22 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 496 ms, Memory: 53468 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
@@ -9,36 +9,29 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(st.nextToken());
-		long[] h;
+		int[] h;
 		long max;
 		Stack<Integer> stack;
 		
 		while (N != 0) {
 			max = 0;
-			h = new long[N+1];
+			h = new int[N];
 			stack = new Stack<>();
 			
-			for (int i=0; i<N; i++) {
-				h[i] = Integer.parseInt(st.nextToken());
-			}
-			
-			int i = 0;
-	        while (i < N) {
-	            if (stack.isEmpty() || h[stack.peek()] <= h[i]) {
-	                stack.push(i++);
-	            } else {
-	                long height = h[stack.pop()];
-	                long width = stack.isEmpty() ? i : i - stack.peek() - 1;
-	                max = Math.max(max, (long) height * width);
-	            }
-	        }
+			for (int i=0; i<=N; i++) {
+				int c = -1;
+				if (i != N) {
+					c = Integer.parseInt(st.nextToken());
+					h[i] = c;
+				}
 
-	        while (!stack.isEmpty()) {
-	            long height = h[stack.pop()];
-	            long width = stack.isEmpty() ? i : i - stack.peek() - 1;
-	            max = Math.max(max, (long) height * width);
-	        }
-	        
+				while (!stack.isEmpty() && h[stack.peek()] > c) {
+					int height = h[stack.pop()];
+					int width = stack.isEmpty() ? i : i-stack.peek()-1;
+					max = Math.max(max, (long) height*width);
+				}
+				stack.push(i);
+			}
 			sb.append(max);
 			
 			st = new StringTokenizer(br.readLine());
