145e02804b8fe52703f00de60b963ed4a01307c4
Seo Geun-Beom <160552393+Geunbeom@users.noreply.github.com>
2025-03-31 08:05:37 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 8.54 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -3,27 +3,21 @@ class Solution {
         long[] answer = new long[numbers.length];
         
         for (int i=0; i<numbers.length; i++) {
-            long k = getBit(numbers[i]);
-            answer[i] = k;
+            if (numbers[i]%2==0) answer[i] = numbers[i]+1;
+            else answer[i] = getBit(numbers[i]);
         }
         
         return answer;
     }
     
     static long getBit(long bit) {
-        if (bit%2 == 0) return bit+1;
-        
-        int k = 0;
         for (int i=0; i<64; i++) {
             if ((bit & ((long) 1<<i)) == 0) {
-                k = i;
-                break;
+                bit |= ((long) 1<<i);
+                bit ^= ((long) 1<<(i-1));
+                return bit;
             }
         }
-
-        bit |= ((long) 1<<k);
-        bit ^= ((long) 1<<(k-1));
-        
         return bit;
     }
     
