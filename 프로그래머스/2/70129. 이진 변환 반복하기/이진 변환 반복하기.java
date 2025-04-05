1b280bcbc954bb95f61d1b01eed9c33e28d8cde2
Seo Geun-Beom <160552393+Geunbeom@users.noreply.github.com>
2025-04-06 06:28:48 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 22.43 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -4,33 +4,13 @@ class Solution {
     
     public int[] solution(String s) {
         int[] answer = new int[2];
-        bit = s;
-        while (bit.length() != 1) {
-            answer[1] += remove(bit);
+        while (s.length() != 1) {
             answer[0]++;
-            bit = makeBit(bit.length());
+            answer[1] += s.length();
+            s = s.replaceAll("0", "");
+            answer[1] -= s.length();
+            s = Integer.toBinaryString(s.length());
         }
         return answer;
     }
-    
-    static int remove(String s) {
-        int count = s.length();
-        bit = s.replace("0", "");
-        count -= bit.length();
-        
-        return count;
-    }
-    
-    static String makeBit(int n) {
-        String k = "";
-        boolean check = false;
-        for (int i=31; i>=0; i--) {
-            if ((n & (1<<i)) != 0) {
-                k += "1";
-                check = true;
-            }else if (check) k += "0";
-        }
-        return k;
-    }
-    
 }
