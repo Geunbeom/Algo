e15c62851255e21beb8c629d735112d767321aed
Seo Geun-Beom <160552393+Geunbeom@users.noreply.github.com>
2025-04-10 18:31:17 +0900

    [Gold IV] Title: 팰린드롬?, Time: 736 ms, Memory: 218708 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -12,50 +12,42 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		int L = Integer.parseInt(br.readLine());
 		int[] arr = new int[L];
+		dp = new boolean[L][L];
 		
 		st = new StringTokenizer(br.readLine());
 		for (int i=0; i<L; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
+			
+			dp[i][i] = true;
+			if (i==0) continue;
+			else if (arr[i] == arr[i-1]) {
+				dp[i-1][i] = true;
+			}
 		}
 		
 		int M = Integer.parseInt(br.readLine());
 		
 		makeFirst(arr, L);
+		
 		for (int i=0; i<M; i++) {
-			sb.append(solution() ? 1 : 0);
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append(dp[a-1][b-1] ? 1 : 0);
 			if (i != M-1) sb.append("\n");
 		}
 		System.out.print(sb);
 	}
 
-	private static boolean solution() throws Exception {
-		st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		
-		return dp[a-1][b-1];
-	}
-
 	private static void makeFirst(int[] arr, int L) {
-		dp = new boolean[L][L];
-		for (int i=0; i<L; i++) {
-			dp[i][i] = true;
-			
-			if (i==L-1) continue;
-			else if (arr[i] == arr[i+1]) {
-				dp[i][i+1] = true;
-			}
-		}
-		
 		for (int l=3; l<=L; l++) {
-			
 			for (int s=0; s<L-l+1; s++) {
 				int e = s+l-1;
 				if (arr[s] != arr[e]) continue;
 				if (dp[s+1][e-1]) dp[s][e] = true;
 			}
 		}
-		
 	}
 
 }
