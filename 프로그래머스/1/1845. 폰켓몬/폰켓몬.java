73165ad8a3f7de841ea5e59e29a30dff57aa66f7
Seo Geun-Beom <160552393+Geunbeom@users.noreply.github.com>
2025-06-28 13:42:44 +0900

    [level 1] Title: 폰켓몬, Time: 3.62 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : nums) set.add(i);
+        return set.size() > nums.length/2 ? nums.length/2 : set.size();
+    }
+}
