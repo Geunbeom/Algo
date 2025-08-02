bfcb52efac0b24c4cd6ccba6d850e16f247559fc
Seo Geun-Beom <160552393+Geunbeom@users.noreply.github.com>
2025-08-03 04:44:45 +0900

    [level 2] Title: 땅따먹기, Time: 20.11 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,27 +1,13 @@
 class Solution {
     int solution(int[][] land) {
-        int answer = 0;
-        int N = land.length;
-        boolean check = false;
-        int[] dp1 = new int[4];
-        int[] dp2 = new int[4];
-        
-        for (int i=0; i<N; i++) {
-            if (check) func(dp1, dp2, land[i]);
-            else func(dp2, dp1, land[i]);
-            check = !check;
+        int N = land.length-1;
+        for (int i=1; i<=N; i++) {
+            land[i][0] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][3]);
+            land[i][1] += Math.max(Math.max(land[i-1][0], land[i-1][2]), land[i-1][3]);
+            land[i][2] += Math.max(Math.max(land[i-1][0], land[i-1][1]), land[i-1][3]);
+            land[i][3] += Math.max(Math.max(land[i-1][0], land[i-1][1]), land[i-1][2]);
         }
-        if (check)
-            return Math.max(Math.max(dp1[0], dp1[1]), Math.max(dp1[2], dp1[3]));
-        else return Math.max(Math.max(dp2[0], dp2[1]), Math.max(dp2[2], dp2[3]));
+        
+        return Math.max(Math.max(land[N][0], land[N][1]), Math.max(land[N][2], land[N][3]));
     }
-    
-    void func(int[] prev, int[] next, int[] land) {
-        next[0] = Math.max(Math.max(prev[1], prev[2]), prev[3]) + land[0];
-        next[1] = Math.max(Math.max(prev[0], prev[2]), prev[3]) + land[1];
-        next[2] = Math.max(Math.max(prev[0], prev[1]), prev[3]) + land[2];
-        next[3] = Math.max(Math.max(prev[0], prev[1]), prev[2]) + land[3];
-    }
-
-
 }
