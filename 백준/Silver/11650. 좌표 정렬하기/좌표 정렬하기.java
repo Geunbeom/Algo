40bdcb8d80eeb4a9019c84b6077312919349e242
Seo Geun-Beom <160552393+Geunbeom@users.noreply.github.com>
2026-02-28 15:10:48 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 540 ms, Memory: 48596 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb;
+    static int N;
+    static PriorityQueue<int[]> pq;
+    static public void main(String[] args) throws Exception {
+        input();
+        System.out.print(sb);
+    }
+
+    static public void input() throws Exception{
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        pq = new PriorityQueue<>((a,b) -> {
+            if (a[0] == b[0]) {
+                return a[1]-b[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+        for (int tc=0; tc<N; tc++) {
+            st = new StringTokenizer(br.readLine());
+            pq.offer(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+        }
+
+        while(!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            sb.append(curr[0]).append(" ").append(curr[1]).append("\n");
+        }
+    }
+
+}
